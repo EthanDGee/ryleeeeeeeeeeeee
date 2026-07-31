d4ad503c2474c024bd917b076bb50f5de53c9954
EthanDGee <workethangee@gmail.com>
2026-07-30 20:21:17 -0600

    fix(parser): added decompression to files .zst files
    Also addressed grammar error in logging statements.

--- server/internal/parser/streamer.go
+++ server/internal/parser/streamer.go
@@ -1,8 +1,11 @@
 package parser
 
 import (
+	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"server/rest/internal/config"
 	"server/rest/internal/database"
@@ -10,12 +13,44 @@ import (
 	"server/rest/internal/utils"
 
 	"github.com/corentings/chess/v2"
+	"github.com/klauspost/compress/zstd"
 )
 
-func ProcessFile(metadata models.Metadata) error {
-	filepath := config.FILES_FOLDER + metadata.Filename
+func DecompressPGN(metadata models.Metadata) {
+	compressedPath := filepath.Join(config.FILES_FOLDER, metadata.Filename)
+	decompressedPath := strings.TrimSuffix(compressedPath, ".zst")
 
-	file, err := os.Open(filepath)
+	in, err := os.Open(compressedPath)
+	if err != nil {
+		log.Fatalf("failed to open %s: %v", compressedPath, err)
+	}
+	defer utils.Close(in, "compressed file")
+
+	decoder, err := zstd.NewReader(in)
+	if err != nil {
+		log.Fatalf("failed to create zstd reader for %s: %v", compressedPath, err)
+	}
+	defer decoder.Close()
+
+	out, err := os.Create(decompressedPath)
+	if err != nil {
+		log.Fatalf("failed to create %s: %v", decompressedPath, err)
+	}
+	defer utils.Close(out, "decompressed file")
+
+	if _, err := io.Copy(out, decoder); err != nil {
+		log.Fatalf("failed to decompress %s: %v", compressedPath, err)
+	}
+}
+
+func ProcessFile(metadata models.Metadata) error {
+	path := strings.TrimSuffix(filepath.Join(config.FILES_FOLDER, metadata.Filename), ".zst")
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		DecompressPGN(metadata)
+	}
+
+	file, err := os.Open(path)
 	if err != nil {
 		return err
 	}
@@ -35,15 +70,15 @@ func ProcessFile(metadata models.Metadata) error {
 
 		game, err := scanner.ParseNext()
 		if err != nil {
-			log.Fatalf("Failed to process game: %d in %s", gameIndex, metadata.Filename)
+			log.Fatalf("Failed to process game: %d from %s", gameIndex, metadata.Filename)
 		}
 
 		err = database.InsertGame(*game, metadata)
 		if err != nil {
-			log.Printf("failed to insert game %d in %s: %v", gameIndex, metadata.Filename, err)
+			log.Printf("failed to insert game %d from %s: %v", gameIndex, metadata.Filename, err)
 			return err
 		}
-		log.Printf("inserted game %d in %s", gameIndex, metadata.Filename)
+		log.Printf("inserted game %d from %s", gameIndex, metadata.Filename)
 
 		gameIndex++
 	}
