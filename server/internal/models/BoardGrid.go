d4f17793589c4129abd959faadbc1298018918c3
EthanDGee <workethangee@gmail.com>
2026-08-03 17:14:57 -0600

    feat(BoardGrid): created board grid constructor

--- server/internal/models/BoardGrid.go
+++ server/internal/models/BoardGrid.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"errors"
+
+	"github.com/corentings/chess/v2"
+)
+
 type BoardGrid struct {
 	WhitePawn   [][]bool
 	BlackPawn   [][]bool
@@ -13,4 +19,64 @@ type BoardGrid struct {
 	BlackQueen  [][]bool
 	WhiteKing   [][]bool
 	BlackKing   [][]bool
+}
+
+func emptyPieceGrid() [][]bool {
+	boardSize := 8
+	grid := make([][]bool, boardSize)
+	for i := range grid {
+		grid[i] = make([]bool, boardSize)
+	}
+	return grid
+}
+
+func NewBoardGrid(game *chess.Game) (BoardGrid, error) {
+	if game == nil {
+		return BoardGrid{}, errors.New("models: nil game")
+	}
+	pos := game.Position()
+	if pos == nil {
+		return BoardGrid{}, errors.New("models: game has no position")
+	}
+
+	grid := BoardGrid{
+		WhitePawn:   emptyPieceGrid(),
+		BlackPawn:   emptyPieceGrid(),
+		WhiteKnight: emptyPieceGrid(),
+		BlackKnight: emptyPieceGrid(),
+		WhiteRook:   emptyPieceGrid(),
+		BlackRook:   emptyPieceGrid(),
+		WhiteBishop: emptyPieceGrid(),
+		BlackBishop: emptyPieceGrid(),
+		WhiteQueen:  emptyPieceGrid(),
+		BlackQueen:  emptyPieceGrid(),
+		WhiteKing:   emptyPieceGrid(),
+		BlackKing:   emptyPieceGrid(),
+	}
+
+	planes := map[chess.Piece][][]bool{
+		chess.WhitePawn:   grid.WhitePawn,
+		chess.BlackPawn:   grid.BlackPawn,
+		chess.WhiteKnight: grid.WhiteKnight,
+		chess.BlackKnight: grid.BlackKnight,
+		chess.WhiteRook:   grid.WhiteRook,
+		chess.BlackRook:   grid.BlackRook,
+		chess.WhiteBishop: grid.WhiteBishop,
+		chess.BlackBishop: grid.BlackBishop,
+		chess.WhiteQueen:  grid.WhiteQueen,
+		chess.BlackQueen:  grid.BlackQueen,
+		chess.WhiteKing:   grid.WhiteKing,
+		chess.BlackKing:   grid.BlackKing,
+	}
+
+	for square, piece := range pos.Board().SquareMap() {
+		plane, ok := planes[piece]
+		if !ok {
+			continue
+		}
+
+		plane[int(square)/8][int(square)%8] = true
+	}
+
+	return grid, nil
 }
